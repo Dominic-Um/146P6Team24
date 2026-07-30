365140c65419a673002a70ac7e2756604bba2a04
Dominic-Um <dumbrasa@ucsc.edu>
2026-07-29 23:10:03 -0700

    Files + Initial basic_model.

---
+++ models/basic_model.py
@@ -0,0 +1,24 @@
+from models.model import Model
+from tensorflow.keras import Sequential, layers
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.optimizers import RMSprop, Adam
+
+class BasicModel(Model):
+    def _define_model(self, input_shape, categories_count):
+        self.model = Sequential([
+        Rescaling(1./255, input_shape=input_shape),
+        layers.Conv2D(16, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(categories_count, activation='softmax'),
+    ])
+    
+def _compile_model(self):
+    self.model.compile(
+        optimizer=RMSprop(learning_rate=0.001),
+        loss='categorical_crossentropy',
+        metrics=['accuracy'],
+    )
