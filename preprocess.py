c29d90039c5fb5e601180c3f77027a21c5d93707
Dominic-Um <dumbrasa@ucsc.edu>
2026-08-01 23:28:04 -0700

    def get_transfer_datasets and def _define_model & def _compile_model initial versions

--- preprocess.py
+++ preprocess.py
@@ -31,8 +31,28 @@ def get_datasets():
     return train_dataset, validation_dataset, test_dataset
 
 def get_transfer_datasets():
-    # Your code replaces this by loading the dataset
-    # you can use image_dataset_from_directory, similar to how the _split_data function is using it
-    train_dataset, validation_dataset, test_dataset = None, None, None
-    # ...
+    train_directory = 'transfer_data/train'
+    test_directory = 'transfer_data/test'
+
+    print('train dataset:')
+    train_dataset, validation_dataset = image_dataset_from_directory(
+        train_directory,
+        label_mode='categorical',
+        color_mode='rgb',
+        batch_size=batch_size,
+        image_size=image_size,
+        validation_split=validation_split,
+        subset="both",
+        seed=47
+    )
+
+    print('test dataset:')
+    test_dataset = image_dataset_from_directory(
+        test_directory,
+        label_mode='categorical',
+        color_mode='rgb',
+        batch_size=batch_size,
+        image_size=image_size,
+        shuffle=False
+    )
     return train_dataset, validation_dataset, test_dataset
