5fb9eda05d37ab49dbed97f2b599b968517459ac
totoropixels <doggylovesme1@gmail.com>
2026-08-03 02:06:02 -0700

    Updated basic_model, 69% accuracy version

--- models/basic_model.py
+++ models/basic_model.py
@@ -1,9 +1,33 @@
 from models.model import Model
 from tensorflow.keras import Sequential, layers
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
-from tensorflow.keras.optimizers import RMSprop, Adam
+from tensorflow.keras.optimizers import RMSprop
 
 class BasicModel(Model):
+    # Current best accuracy: 69% on 15 epochs
+    def _define_model(self, input_shape, categories_count):
+        self.model = Sequential([
+        Rescaling(1./255, input_shape=input_shape),
+        layers.Conv2D(16, 3, activation='relu'),
+        layers.MaxPooling2D((4,4)),
+        layers.Dropout(0.1),
+
+        layers.Conv2D(32, 3, activation='relu'),
+        layers.MaxPooling2D((3,3)),
+        layers.Dropout(0.1),
+
+        layers.Conv2D(48, 3, activation='relu'),
+        layers.MaxPooling2D((2,2)),
+        layers.Dropout(0.1),
+
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(categories_count, activation='softmax'),
+    ])
+
+    """
+    Initial Version below. 60% accuracy on 6 epochs in train.py
+    ---------------------------------------------
     def _define_model(self, input_shape, categories_count):
         self.model = Sequential([
         Rescaling(1./255, input_shape=input_shape),
@@ -15,10 +39,12 @@ class BasicModel(Model):
         layers.Dense(64, activation='relu'),
         layers.Dense(categories_count, activation='softmax'),
     ])
+    """
+
     
-def _compile_model(self):
-    self.model.compile(
-        optimizer=RMSprop(learning_rate=0.001),
-        loss='categorical_crossentropy',
-        metrics=['accuracy'],
-    )
+    def _compile_model(self):
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'],
+        )
